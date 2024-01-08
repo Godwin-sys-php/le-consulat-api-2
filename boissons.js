8a53a554436550ea6519b3991dc3d0cab5b3b9fc
Godwin Burume <godwinnyembo@gmail.com>
2024-01-08 16:33:14 +0100

    spiritus sql

--- boissons.js
+++ boissons.js
@@ -1,15 +1,20 @@
 const fs = require('fs');
 const csv = require('csv-parser');
 
-const sqlFile = fs.createWriteStream('boissons.sql');
+const sqlFile = fs.createWriteStream('spiritus.sql');
 
-fs.createReadStream('boissons.csv')
+fs.createReadStream('spiritus.csv')
   .pipe(csv({separator: ';'}))
   .on('data', (row) => {
     console.log(row);
     // Créer la requête INSERT pour chaque ligne
     if (Number(row.Reel) > 0) {
-      const insertQuery = `UPDATE products SET inStock = ${Number(row.Reel)} where name='${row.Nom}';\n`;
+      const insertQuery = `UPDATE products SET inStock = ${Number(row.Reel)} where name="${row.Nom}";\n`;
+      
+      // Écrire la requête dans le fichier SQL
+      sqlFile.write(insertQuery);
+    } else {
+      const insertQuery = `UPDATE products SET inStock = ${0} where name="${row.Nom}";\n`;
       
       // Écrire la requête dans le fichier SQL
       sqlFile.write(insertQuery);
